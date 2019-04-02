272b17b2d78e5bee61ee7fcd51a315445d9485d3
nardsqq <nardparagas@gmail.com>
2019-04-02 22:09:11 +0800

    Add static variable registrationRules to set an array of validation rules for registration

--- app/User.php
+++ app/User.php
@@ -37,4 +37,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static $registrationRules = [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6|confirmed'
+    ];
 }
