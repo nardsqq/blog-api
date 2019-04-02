1a0c16bb72d159d3381c2c4d19ea2825afe9f46d
nardsqq <nardparagas@gmail.com>
2019-04-02 15:41:35 +0800

    Add an AuthController to handle authentication requests

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Journey\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Inspect request to properly login user to the application's APIs
+     * 
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        
+    }
+}
