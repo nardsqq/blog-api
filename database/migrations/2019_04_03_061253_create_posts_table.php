cbdbe92c7f4a011d807f9725beb875812e4351c2
nardsqq <nardparagas@gmail.com>
2019-04-03 14:18:56 +0800

    Add categories, tags, posts, and intermediary post_tag table for the database

---
+++ database/migrations/2019_04_03_061253_create_posts_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('body');
+
+            $table->unsignedBigInteger('category_id');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropForeign(['category_id']);
+        });
+
+        Schema::dropIfExists('posts');
+    }
+}
