c53abcc93ef2d46077d004639d842f502a34da6d
nardsqq <nardparagas@gmail.com>
2019-04-03 14:30:11 +0800

    Attach a hasMany relationship to the Category model relating to the Post model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,17 @@ namespace Journey\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+use Journey\Models\Post;
+
 class Category extends Model
 {
     use SoftDeletes;
 
     protected $guarded = [];
     protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
