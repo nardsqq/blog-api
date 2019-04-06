b8f05017246cf21233b29e7d62025a325f3ffb86
nardsqq <nardparagas@gmail.com>
2019-04-07 01:20:36 +0800

    Add a functionality to the destroy() method

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -75,11 +75,15 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Post $post
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Post $post)
     {
-        //
+        if (!$post->delete()) {
+            return response()->json("Failed to delete specified post.");
+        }
+
+        return response()->json([], 204);
     }
 }
